a08e49ce95d74e84bfb28f9cfddb87f05ab298b1
chfw <wangc_2011@hotmail.com>
2020-01-11 23:03:49 +0000

    :microscope: test #131

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -11,7 +11,29 @@ def test_setup():
         content = f.read()
     yaml = YAML(typ='safe')
     context = yaml.load(content)
-    print(context)
     rendered = get_rendered_file(filename, context)
     expected = get_file_content('setup.py.output')
+    assert expected == rendered
+
+
+def test_setup_use_markers_true():
+    filename = 'setup.py.jj2'
+    with open(os.path.join("tests", "fixtures", "server_use_marker_true.yml"))as f:
+        content = f.read()
+    yaml = YAML(typ='safe')
+    context = yaml.load(content)
+    rendered = get_rendered_file(filename, context)
+    print(rendered)    
+    expected = get_file_content('setup_use_markers_true.py.output')
+    assert expected == rendered
+
+
+def test_setup_use_markers_false():
+    filename = 'setup.py.jj2'
+    with open(os.path.join("tests", "fixtures", "server_use_marker_false.yml"))as f:
+        content = f.read()
+    yaml = YAML(typ='safe')
+    context = yaml.load(content)
+    rendered = get_rendered_file(filename, context)
+    expected = get_file_content('setup_use_markers_false.py.output')
     assert expected == rendered
